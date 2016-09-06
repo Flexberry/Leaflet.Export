81cecf51d791600ccc161a5aa630657b0fd8bb14
kaf <kaf@nevod.ru>
2016-09-06 16:43:58 +0500

    Initial support

--- leaflet_export.js
+++ leaflet_export.js
@@ -4,16 +4,32 @@
    * Базовое пространство имен для инструментов экпорта.
    */
   L.Export =  L.Map.extend({
-    initialize: function (options) {
+    initialize: function (map, options) {
+      L.Map.prototype.initialize.call(this, map, options);
     },
+
+    export: function(options){
+      options = options || {};
+      var mapId = options.mapId || 'map';
+      var mapElement = document.getElementById(mapId);
+      var promise = html2canvas(mapElement, {
+        allowTaint: true,
+        logging: true,
+      });
+      promise.then(function(canvas) {
+        var newCanvas = canvas;
+      }, function(reason) {
+        var newReason = reason;
+      });
+    }
 
   });
 
- /*
-  Фабричный метод для создания базового экземпляра.
-  */
-    L.export = function(options) {
-      return new L.Export(options);
-    };
+  /*
+    Фабричный метод для создания базового экземпляра.
+    */
+  L.export = function(map, options) {
+    return new L.Export(map, options);
+  };
 
 })(L);
