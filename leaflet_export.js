3f6b0f8e5901cc75c5cc6dcfeb35d30bc055c730
kaf <kaf@nevod.ru>
2016-09-09 15:38:22 +0500

    Partially options support

--- leaflet_export.js
+++ leaflet_export.js
@@ -5,19 +5,61 @@
    */
   L.Export =  L.Map.extend({
     initialize: function (map, options) {
+      options = options || {};
+      options.preferCanvas = true;
       L.Map.prototype.initialize.call(this, map, options);
     },
 
     export: function(options){
-      options = options || {};
-      var mapId = options.mapId || 'map';
-      var mapElement = document.getElementById(mapId);
+      var caption = {};
+      var exclude = [];
+      options = options || { caption: {}, exclude: []};
+      if ('caption' in options) {
+        caption = options['caption'];
+        if ('position' in caption) {
+          var position = caption.position;
+          if (!Array.isArray(position) && position.length !=2 )
+            if (position.length == 0 || typeof position[0] !== 'number'){
+              caption.position[0] = 0;
+            }
+
+            if (position.length == 1 || typeof position[1] !== 'number') {
+              caption.position[1] = 0;
+            }
+        }
+      }
+
+      if ('exclude' in options && Array.isArray(options['exclude'])) {
+        exclude = options['exclude'];
+      }
+
+      var mapElement = this._container;
+      var _this = this;
       var promise = html2canvas(mapElement, {
         allowTaint: true,
+        useCORS: true,
         logging: true,
-      });
-      promise.then(function(canvas) {
-        var newCanvas = canvas;
+      }).then(function(canvas) {
+        if ('text' in caption) {
+          var x, y;
+          if ('position' in caption) {
+            x = caption.position[0];
+            y = caption.position[1];
+          } else {
+            x = 0;
+            y = 0;
+          }
+          var ctx = canvas.getContext('2d');
+          if ('font' in caption) {
+            ctx.font = caption.font;
+          }
+          if ('fillStyle' in caption) {
+            ctx.fillStyle = caption.fillStyle;
+          }
+          ctx.fillText(caption.text,x , y);
+        }
+
+        document.body.appendChild(canvas);
       }, function(reason) {
         var newReason = reason;
       });
