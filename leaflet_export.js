6012a591d3667aa7944d3f5757076b2c0dd62b52
kaf <kaf@nevod.ru>
2016-09-12 23:59:58 +0500

    Move from map properties to map methods

--- leaflet_export.js
+++ leaflet_export.js
@@ -1,130 +1,102 @@
 (function(L, undefined) {
-
-  /**
-   * Базовое пространство имен для инструментов экпорта.
-   */
-  L.Export =  L.Class.extend({
-    initialize: function (map, options) {
-      options = options || {};
-      this.options = options;
-      this._map = map;
-      if (!this._map.exportTool) {
-        this._map.exportTool = this;
-      }
-    },
-
-    export: function(options){
-      L.Util.setOptions(this, options);
-      var caption = {};
-      var exclude = [];
-      var format ='image/png';
-      options = options || { caption: {}, exclude: []};
-      if ('caption' in options) {
-        caption = options['caption'];
-        if ('position' in caption) {
-          var position = caption.position;
-          if (!Array.isArray(position) && position.length !=2 )
-            if (position.length == 0 || typeof position[0] !== 'number'){
-              caption.position[0] = 0;
-            }
-
-            if (position.length == 1 || typeof position[1] !== 'number') {
-              caption.position[1] = 0;
-            }
-        }
-      }
-
-      if ('exclude' in options && Array.isArray(options['exclude'])) {
-        exclude = options['exclude'];
-      }
-
-      if ('format' in options) {
-        format = options['format'];
-      }
-
-      var hide = [];
-      for (var i = 0; i < exclude.length; i++) {
-        var selector = exclude[i];
-        switch (typeof selector) {
-          case 'object':
-            if ('tagName' in selector) {  //DOM element
-              hide.push(selector);
-            }
-            break;
-          case 'string':  //selector
-            var type = selector.substr(0,1);
-            switch (type) {
-              case '.': //class selector
-                var elements = this._map._container.getElementsByClassName(selector.substr(1));
-                for (var j = 0; j < elements.length; j++) {
-                  hide.push(elements.item(j));
-                }
-                break;
-              case '#':   //id selector
-                var element = this._map._container.getElementById(selector.substr(1));
-                if (element) {
-                  hide.push(element);
-                }
-            }
-        }
-      }
-      for (var i = 0; i < hide.length; i++) { //Hide exclude elements
-        hide[i].setAttribute('data-html2canvas-ignore', 'true');
-      }
-      var _this = this;
-      return html2canvas(this._map._container, {
-//         allowTaint: true,
-        useCORS: true,
-        logging: true,
-      }).then(function(canvas) {
-        for (var i = 0; i < hide.length; i++) { //Unhide exclude elements
-          hide[i].setAttribute('data-html2canvas-ignore', 'false');
-        }
-        if ('text' in caption && caption.text) {
-          var x, y;
-          if ('position' in caption) {
-            x = caption.position[0];
-            y = caption.position[1];
-          } else {
-            x = 0;
-            y = 0;
-          }
-          var ctx = canvas.getContext('2d');
-          if ('font' in caption) {
-            ctx.font = caption.font;
-          }
-          if ('fillStyle' in caption) {
-            ctx.fillStyle = caption.fillStyle;
-          }
-          ctx.fillText(caption.text,x , y);
-        }
-
-        document.body.appendChild(canvas);
-        var ret = format === 'canvas' ? canvas : { data:canvas.toDataURL(format), width: canvas.width,  height: canvas.height, type: format};
-        return ret;
-      }, function(reason) {
-        var newReason = reason;
-        alert(reason);
-      });
-    }
-
-  });
-
-  /*
-    Фабричный метод для создания базового экземпляра.
-    */
-  L.export = function(map, options) {
-    return new L.Export(map, options);
-  };
-
-  L.Map.mergeOptions({
-    preferCanvas: true
-  });
-
   L.Map.addInitHook(function () {
     this.whenReady(function () {
-      if (this.options.exportable || this.options.exportOptions) {
-        this.exportTool = new L.Export(this, this.options.exportOptions);
+      if (this.options.exportable || this.options.printable || this.options.downloadable) {
+        L.Map.mergeOptions({ preferCanvas: true });
+//         this.exportTool = new L.Export(this, this.options.exportOptions);
+        this.export = function(options){
+          var caption = {};
+          var exclude = [];
+          var format ='image/png';
+          options = options || { caption: {}, exclude: []};
+          if ('caption' in options) {
+            caption = options['caption'];
+            if ('position' in caption) {
+              var position = caption.position;
+              if (!Array.isArray(position) && position.length !=2 )
+                if (position.length == 0 || typeof position[0] !== 'number'){
+                  caption.position[0] = 0;
+                }
+
+                if (position.length == 1 || typeof position[1] !== 'number') {
+                  caption.position[1] = 0;
+                }
+            }
+          }
+
+          if ('exclude' in options && Array.isArray(options['exclude'])) {
+            exclude = options['exclude'];
+          }
+
+          if ('format' in options) {
+            format = options['format'];
+          }
+
+          var hide = [];
+          for (var i = 0; i < exclude.length; i++) {
+            var selector = exclude[i];
+            switch (typeof selector) {
+              case 'object':
+                if ('tagName' in selector) {  //DOM element
+                  hide.push(selector);
+                }
+                break;
+              case 'string':  //selector
+                var type = selector.substr(0,1);
+                switch (type) {
+                  case '.': //class selector
+                    var elements = this._container.getElementsByClassName(selector.substr(1));
+                    for (var j = 0; j < elements.length; j++) {
+                      hide.push(elements.item(j));
+                    }
+                    break;
+                  case '#':   //id selector
+                    var element = this._container.getElementById(selector.substr(1));
+                    if (element) {
+                      hide.push(element);
+                    }
+                }
+            }
+          }
+          for (var i = 0; i < hide.length; i++) { //Hide exclude elements
+            hide[i].setAttribute('data-html2canvas-ignore', 'true');
+          }
+          var _this = this;
+          return html2canvas(this._container, {
+            //         allowTaint: true,
+            useCORS: true,
+            logging: true,
+          }).then(function(canvas) {
+            for (var i = 0; i < hide.length; i++) { //Unhide exclude elements
+              hide[i].setAttribute('data-html2canvas-ignore', 'false');
+            }
+            if ('text' in caption && caption.text) {
+              var x, y;
+              if ('position' in caption) {
+                x = caption.position[0];
+                y = caption.position[1];
+              } else {
+                x = 0;
+                y = 0;
+              }
+              var ctx = canvas.getContext('2d');
+              if ('font' in caption) {
+                ctx.font = caption.font;
+              }
+              if ('fillStyle' in caption) {
+                ctx.fillStyle = caption.fillStyle;
+              }
+              ctx.fillText(caption.text,x , y);
+            }
+
+            document.body.appendChild(canvas);
+            var ret = format === 'canvas' ? canvas : { data:canvas.toDataURL(format), width: canvas.width,  height: canvas.height, type: format};
+            return ret;
+          }, function(reason) {
+            var newReason = reason;
+            alert(reason);
+          });
+        };
       }
     });
   });
--- leaflet_export_print.js
+++ leaflet_export_print.js
@@ -1,64 +1,36 @@
 (function(L, undefined) {
-
-  /**
-   * Базовое пространство имен для инструментов экпорта.
-   */
-  L.Export.Print =  L.Export.extend({
-    errors: {
-      popupWindowBlocked: 'Окно печати было заблокировано браузером. Пожалуйста разрешите всплывающие окна на этой странице',
-    },
-
-    initialize: function (map, options) {
-      options = options || {};
-      this.options = options;
-      L.Export.prototype.initialize.call(this, map, options);
-      this._map = map;
-      if (!this._map.exportPrintTool) {
-        this._map.exportPrintTool = this;
-      }
-    },
-
-    printExport: function(options) {
-      L.Util.setOptions(this, options);
-      var _this = this;
-      this.export(this.options).then(
-        function(result) {
-          var printWindow = window.open('', '_blank');
-          if (printWindow) {
-            var printDocument = printWindow.document;
-            printDocument.write('<html><head><title>' + (options.text ? options.text : '') + '</title></head><body onload=\'window.print(); window.close();\'></body></html>');
-
-            var img = printDocument.createElement('img');
-            img.height = result.height;
-            img.width = result.width;
-            img.src = result.data;
-
-            printDocument.body.appendChild(img);
-
-            printDocument.close();
-            printWindow.focus();
-          } else {
-            throw new Error(this.errors.popupWindowBlocked);
-          }
-
-        }
-      );
-
-    }
-
-  });
-
-  /*
-    Фабричный метод для создания базового экземпляра.
-    */
-  L.export.print = function(map, options) {
-    return new L.Export.Print(map, options);
-  };
-
   L.Map.addInitHook(function () {
     this.whenReady(function () {
-      if (this.options.printable || this.options.printOptions) {
-        this.exportPrintTool = new L.Export.Print(this, this.options.printOptions);
+      if (this.options.printable) {
+        if (!('exportError' in this)) {
+          this.exportError = {};
+        }
+        this.exportError.popupWindowBlocked = 'Окно печати было заблокировано браузером. Пожалуйста разрешите всплывающие окна на этой странице';
+        this.printExport = function(options) {
+          var _this = this;
+          this.export(options).then(
+            function(result) {
+              var printWindow = window.open('', '_blank');
+              if (printWindow) {
+                var printDocument = printWindow.document;
+                printDocument.write('<html><head><title>' + (options.text ? options.text : '') + '</title></head><body onload=\'window.print(); window.close();\'></body></html>');
+
+                var img = printDocument.createElement('img');
+                img.height = result.height;
+                img.width = result.width;
+                img.src = result.data;
+
+                printDocument.body.appendChild(img);
+
+                printDocument.close();
+                printWindow.focus();
+              } else {
+                throw new Error(_this.exportError.popupWindowBlocked);
+              }
+
+            }
+          );
+        };
       }
     });
   });
